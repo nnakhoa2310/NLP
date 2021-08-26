790a65886f019faeb7e672b25c52c0a691296513
nnakhoa2310 <81813890+nnakhoa2310@users.noreply.github.com>
2021-08-26 11:28:09 +0700

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
 from textblob import TextBlob
 from nltk.corpus import stopwords
 from spellchecker import SpellChecker
@@ -17,21 +19,34 @@ for sense in getdiv:
 text_r = re.sub(r'[^\w\s]','',text).lower()
 # remove stop words
 list_stop_words = stopwords.words('english')
-final_text = {text_r.replace(stop_word, '') for stop_word in list_stop_words}
+final_list = []
+word_tokens = nltk.word_tokenize(str(text_r))
+for w in word_tokens:
+    if w not in list_stop_words:
+        final_list.append(w)
+final_text = ' '.join(map(str, final_list))
 # spell checker
 spell = SpellChecker()
-misspelled = spell.unknown(str(final_text))
+misspelled = spell.unknown(final_text)
 # for word in misspelled:
 #     print(word)
 # detection language
-b = TextBlob(str(final_text))
+b = TextBlob(final_text)
 # print(b.detect_language())
 # tokenize
-# print(nltk.word_tokenize(str(final_text)))
+# print(nltk.word_tokenize(final_text))
 # count the number of occurrences of a word
 str(final_text).count("python")
 len(final_text)
 # write file
-f = open('mytext.txt', "w")
-f.write(str(final_text))
-f.close()
+# f = open('mytext.txt', "w")
+# f.write(final_text)
+# f.close()
+# lemmatizer and stemmer
+wordnet_lemmatizer = WordNetLemmatizer()
+stemer = PorterStemmer()
+sentence_words = nltk.word_tokenize(final_text)
+print("{0:20}{1:20}{2:20}".format("original", "lemmatize", "stememer"))
+for word in sentence_words:
+    print("{0:20}{1:20}{2:20}".format(word, wordnet_lemmatizer.lemmatize(word, pos="v"),
+    stemer.stem(word)))
